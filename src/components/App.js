0c21afcd1105d9f68330e33c12288d443af80751
Artyom K <artem@belka.app>
2020-12-04 21:00:02 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import { Alert, Button, Container, Nav } from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <div className="App"> 
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Button>Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
